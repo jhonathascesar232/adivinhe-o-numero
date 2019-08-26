19e0daa2b3ff6fdfdffd2b5e82522f2504a63ac9
Jhonathas César <jhonathascesar232@gmail.com>
2019-08-26 20:01:44 -0300

    Upando o game

---
+++ Adivinhe_o_numero.py
@@ -0,0 +1,103 @@
+from random import randint
+
+
+def dificuldade():
+        nivel = 0
+        print("""
+**
+** Escolha o nivél:
+**
+** 1 - Fácil
+** 2 - Normal
+** 3 - Difícil
+** 0 - Sair""")
+        op = int(input("** --> "))        
+        while True:
+                
+                if (op == 1):
+                        nivel = 20
+                        break                        
+                elif (op == 2):
+                        nivel = 10
+                        break
+                elif (op == 3):
+                        nivel = 5
+                        break
+                elif (op == 0):
+                        nivel = 0
+                        break
+                else:
+                        print("** Opção inválida!. Tente Novamente.")
+                        op = int(input("** --> "))
+        return nivel
+
+
+def run():
+        numero_sorteado = randint(0 ,101)
+        nivel = dificuldade()
+        tentativas = 1
+        diferenca = 0
+        cont = nivel
+        PONTOS = 1000
+        msn = ""
+
+        while True:
+                
+                if (nivel == 0):
+                        break
+
+                cont -= 1
+                
+                print("\n** PONTOS --> {}\t TENTATIVA --> {}".format(PONTOS, tentativas))
+                chute = int(input("** Chute um número: "))
+                
+                if (tentativas == nivel):
+                        print("** Você PERDEU.")
+                        break
+                        
+                if (chute == numero_sorteado):
+                        print("\n**")
+                        print("** Parabéns VOCÊ acertou o número")
+                        print("** O número sorteado foi {}".format(numero_sorteado))
+                        print("** Você acertou em {} tentativas.".format(tentativas))
+                        break
+                elif (chute > numero_sorteado):
+                        diferenca = chute - numero_sorteado
+                        PONTOS -= diferenca
+                        print("\n** Você ERROU. Faltam {} tentativas.".format(cont))
+                        print("** Tente novamente.")
+                        print("** SEU NÚMERO É MAIOR DO QUE O NÚMERO SORTEADO!")
+                elif (chute < numero_sorteado):
+                        diferenca = numero_sorteado - chute
+                        PONTOS -= diferenca
+                        print("\n** Você ERROU. Faltam {} tentativas.".format(cont))
+                        print("** Tente novamente.")
+                        print("** SEU NÚMERO É MENOR DO QUE O NÚMERO SORTEADO!")
+
+                
+                tentativas += 1
+
+
+def main():
+        print("""
+** Iniciar jogo?
+** (y/n)
+**""")
+        op = input("** -->: ")
+        while True:
+                if (op == "n"):
+                        print("\n** FIM da execução...")
+                        break
+                elif (op == "y"):
+                        run()
+                else:
+                        print("\n** Valores POSSIVEIS \n** ( y/n )")
+                print("""
+** Quer continuar jogando?
+** (y) --> SIM
+** (n) --> NÂO
+**""") 
+                op = input("** -->  ")
+
+if __name__ == "__main__":
+        main()
